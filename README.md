0034dba0666156d86a36816f8cdbb19d3cecf2df
VadimVelikanets <velikanets@mail.ru>
2020-10-15 03:00:09 +0300

    test task

---
+++ README.md
@@ -0,0 +1,29 @@
+# OZiTAG – Test task
+
+## Описание
+
+В данном тестовом задании использованы  технологии JQuery, Sass, Gulp, Pug. 
+
+## Ссылка на демонстрацию
+```
+http://fincom24.seobility.by/
+```
+
+## Скачивание
+
+```
+git clone {repository URL}
+```
+
+## Установка
+
+```
+npm install
+```
+
+## Запуск
+
+```
+gulp start
+```
+
--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,25 @@
 $(function() {
-
+    //Открытие меню
+    $('.hamburger__btn').click(
+        function(e){
+            e.preventDefault();
+            $('.header__menu').slideToggle();
+            $(this).find('img').slideToggle(0);
+        }
+    )
+    //Открытие пунктов меню на мобилом
+    $('.header__menu .btn-arrow').click(
+        function(){
+            if($(this).parent().parent().hasClass('active')){
+                $(this).parent().parent().removeClass('active')
+            } else{
+                $(this).parent().parent().addClass('active')
+            }
+            let activeBlock = $(this);
+            $('html').animate({
+                scrollTop: $(activeBlock).offset().top - 20
+            }, 500);
+        }
+    )
 
 })
